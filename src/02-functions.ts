5358713534476236d74d713b26032def283e761a
Aaron Delaney <20092443@mail.wit.ie>
2023-11-20 12:05:04 +0000

    Higher-order functions

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,4 +1,4 @@
-import {Friend, Colleague } from './myTypes'
+import {Friend, Colleague, EmailContact } from './myTypes'
 import { friends, colleagues } from "./01-basics";
 
 function older(f: Friend) : string {
@@ -10,7 +10,7 @@ function allOlder(friends: Friend[]) : string[] {
     return friends.map(f =>  `${f.name} is now ${f.age + 1}`)
 }
 
-function highestExtension(colleagues: Colleague[]): Colleague {
+function highestExtension(colleagues: Colleague[]) {
     const result = colleagues.sort(
       (c1, c2) => c1.contact.extension - c2.contact.extension
     );
@@ -33,10 +33,28 @@ function addColleague(
     };
     colleagues.push(newColleague);
 }
- 
-addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
-console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
 
+function sortColleagues(
+    colleagues: Colleague[],
+    sorter: (c1: Colleague, c2: Colleague) => number
+  ): EmailContact[] {
+    const sorted = colleagues.sort(sorter); // Colleague[] inferred
+    const result: EmailContact[] = sorted.map((ce) => ({ name: ce.name, email: ce.contact.email }));
+    return result 
+  }
+  
+  
+  function findFriends(friends: Friend[], invocation: (f: Friend) => boolean) : string[] {
+    return friends.filter(invocation).map(f => f.name)
+}
+
+addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
+
+console.log(sortColleagues(colleagues.current, (a, b) => a.contact.extension - b.contact.extension));
+console.log(sortColleagues(colleagues.current, (a, b) => a.name.length - b.name.length));
+console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
+console.log(findFriends(friends, (friend) => friend.age < 35));
+console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
 console.log(older(friends[0]))
 console.log(allOlder(friends))
 console.log(highestExtension(colleagues.current));
